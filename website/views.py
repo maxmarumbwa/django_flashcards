ce1d805e83b7a2c83421c1206330ba268fe7bd1f
maxmarumbwa <maxmarumbwa@gmail.com>
2025-05-19 14:02:29 +0200

    initial commit

---
+++ website/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, "home.html", {})
+
+
+def add(request):
+    from random import randint
+
+    num_1 = randint(0, 10)
+    num_2 = randint(0, 10)
+
+    if request.method == "POST":
+        answer = request.POST["answer"]
+        old_num_1 = request.POST["old_num_1"]
+        old_num_2 = request.POST["old_num_2"]
+
+        correct_answer = int(old_num_1) + int(old_num_2)
+        if int(answer) == correct_answer:
+            my_answer = "Correct" + " " + old_num_1 + " + " + old_num_2 + " = " + answer
+            color = "success"
+        else:
+            my_answer = (
+                "Incorrect" + " " + old_num_1 + " + " + old_num_2 + " = " + answer
+            )
+            color = "danger"
+
+        return render(
+            request,
+            "add.html",
+            {
+                "answer": answer,
+                "my_answer": my_answer,
+                "num_1": num_1,
+                "num_2": num_2,
+                "color": color,
+            },
+        )
+
+    return render(
+        request,
+        "add.html",
+        {
+            "num_1": num_1,
+            "num_2": num_2,
+        },
+    )
+
+
+def subtract(request):
+    return render(request, "subtract.html", {})
+
+
+def divide(request):
+    return render(request, "divide.html", {})
+
+
+def multiply(request):
+    return render(request, "multiply.html", {})
